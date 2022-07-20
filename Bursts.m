2eb4618577aae52aadc75d50af508fea7ec78d6a
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-07-20 12:22:18 +0200

    plots for ebrs poster updates

--- Bursts.m
+++ Bursts.m
@@ -26,8 +26,6 @@ TitleTag = 'Bursts';
 
 MegatTable_Filename = [Task, 'AllBursts.mat'];
 
-load(fullfile(Paths.Data, 'EEG', 'Bursts_Table', MegatTable_Filename), 'BurstTable', 'Missing')
-
 Path = fullfile(Paths.Data, 'EEG', 'Bursts_Table');
 Tasks = {'Fixation', 'Oddball', 'Standing'};
 [AllBursts, AllMissing] = loadRRTBursts(Path, Tasks);
@@ -60,17 +58,17 @@ for Indx_T = 1:numel(Tasks)
                 Variable =Variables{Indx_V};
                 Matrix = table2matrixRRT(AllBursts(AllBursts.FreqType == Indx_B, :), AllMissing, Variable, Participants, Sessions, Tasks, Z);
 
-%                 figure('Units','normalized', 'Position', [0 0 .5 .7])
+                %                 figure('Units','normalized', 'Position', [0 0 .5 .7])
                 figure('units', 'centimeters', 'position', [0 0 30 30])
                 plotBrokenSpaghetti(squeeze(Matrix(:, :, Indx_T)), [], [], StatsP, PlotProps.Color.Participants, PlotProps)
 
                 Title = strjoin({replace(Variable, '_', ' '), Bands{Indx_B}, Tasks{Indx_T}}, ' ');
-%                 title(Title,  'FontSize', PlotProps.Text.TitleSize)
+                %                 title(Title,  'FontSize', PlotProps.Text.TitleSize)
                 ylabel(YLabels{Indx_T})
 
                 if Z
                     ylabel('z-scores')
-%                     ylim(YLims)
+                    %                     ylim(YLims)
                     Score =  'zscore';
 
                     % fit data
@@ -78,18 +76,18 @@ for Indx_T = 1:numel(Tasks)
                     Struct = fitStruct(X, Y, WMZ, TestPoint);
                     Struct.Variable = [Variable, '_', Bands{Indx_B}, '_', Task];
                     Fits = catStruct(Fits, Struct);
-                    
+
                 else
                     Score = 'raw';
                 end
 
                 saveFig(strjoin({TitleTag, 'AllSessions', replace(Title, ' ', '_'), Score}, '_'), Results, PlotProps)
 
-%                 % look at gender differences as well
-%                 figure('Units','normalized', 'Position', [0 0 .5 .7])
-%                 Stats = groupDiff(Matrix(:, :, Indx_T), Labels.Sessions, [], [], Colors, StatsP, PlotProps);
-%                 title(Title, 'FontSize', PlotProps.Text.TitleSize)
-%                 saveFig(strjoin({TitleTag, 'Gender', 'BySession', replace(Title, ' ', '_'), Score}, '_'), Results, PlotProps)
+                %                 % look at gender differences as well
+                %                 figure('Units','normalized', 'Position', [0 0 .5 .7])
+                %                 Stats = groupDiff(Matrix(:, :, Indx_T), Labels.Sessions, [], [], Colors, StatsP, PlotProps);
+                %                 title(Title, 'FontSize', PlotProps.Text.TitleSize)
+                %                 saveFig(strjoin({TitleTag, 'Gender', 'BySession', replace(Title, ' ', '_'), Score}, '_'), Results, PlotProps)
             end
         end
     end
@@ -183,40 +181,41 @@ saveFig(strjoin({TitleTag, 'WMZ', 'HedgesG'}, '_'), Results, PlotProps)
 
 Bands = {'Theta', 'Alpha'};
 
+BurstTable = AllBursts(strcmp(AllBursts.Task, 'Fixation'), :);
 
 load('Chanlocs123.mat')
 Topos = nan(numel(Participants), numel(Sessions), numel(Chanlocs), 2, 2);
 
 
+for Indx_T = 1:numel(Tasks)
+    for Indx_S = 1:numel(Sessions)
+        for Indx_B = 1:2 % loop through frequencies
+            for Indx_P = 1:numel(Participants)
+                T = BurstTable(BurstTable.FreqType == Indx_B & strcmp(BurstTable.Participant, Participants{Indx_P}) & ...
+                    strcmp(BurstTable.Session, Sessions{Indx_S}), :);
 
-for Indx_S = 1:numel(Sessions)
-    for Indx_B = 1:2 % loop through frequencies
-        for Indx_P = 1:numel(Participants)
-            T = BurstTable(BurstTable.FreqType == Indx_B & strcmp(BurstTable.Participant, Participants{Indx_P}) & ...
-                strcmp(BurstTable.Session, Sessions{Indx_S}), :);
-
-            if isempty(T)
-                continue
-            end
-
-            Ch = [T.Coh_Burst_Channels{:}];
-            Amps = [T.Coh_amplitude{:}];
-
-            for Indx_Ch = 1:numel(Chanlocs)
-
-                Ch_Mean = mean(Amps(Ch==Indx_Ch), 'omitnan');
-                Ch_Tot = nnz(Amps(Ch==Indx_Ch));
-
-                if Ch_Tot == 0
-                    Ch_Mean = 0;
+                if isempty(T)
+                    continue
                 end
-                Topos(Indx_P, Indx_S, Indx_Ch, Indx_B, 1) = Ch_Mean;
-                Topos(Indx_P, Indx_S, Indx_Ch, Indx_B, 2)  = Ch_Tot;
+
+                Ch = [T.Coh_Burst_Channels{:}];
+                Amps = [T.Coh_amplitude{:}];
+
+                for Indx_Ch = 1:numel(Chanlocs)
+
+                    Ch_Mean = mean(Amps(Ch==Indx_Ch), 'omitnan');
+                    Ch_Tot = nnz(Amps(Ch==Indx_Ch));
+
+                    if Ch_Tot == 0
+                        Ch_Mean = 0;
+                    end
+                    Topos(Indx_P, Indx_S, Indx_Ch, Indx_B, 1) = Ch_Mean;
+                    Topos(Indx_P, Indx_S, Indx_Ch, Indx_B, 2)  = Ch_Tot;
+                end
             end
         end
     end
 end
-
 
 zTopos(:, :, :, :, 1) = zScoreData(squeeze(Topos(:, :, :, :, 1)), 'last');
 zTopos(:, :, :, :, 2) = zScoreData(squeeze(Topos(:, :, :, :, 2)), 'last');
@@ -288,3 +287,39 @@ for Z = [false, true]
 
     saveFig(strjoin({TitleTag, 'WMZ', 'Topography', 'Changes', zTag}, '_'), Results, PlotProps)
 end
+
+
+%% plot topos for EBRS poster
+
+CLims = [-8 8];
+notWMZ_Indx = 7:8;
+WMZ_Indx = 9:10;
+Variables = {'Amplitude', 'Quantity'};
+
+for Indx_V = 1:2
+    T = zTopos;
+
+    figure('Units', 'normalized', 'Position', [0 0 .4 .7])
+    for Indx_B = 1:2
+
+        % first to last recording
+        Data1 = squeeze(mean(T(:, notWMZ_Indx, :, Indx_B, Indx_V), 2, 'omitnan'));
+        Data2 = squeeze(mean(T(:, WMZ_Indx, :, Indx_B, Indx_V), 2, 'omitnan'));
+        subfigure([], [2, 2], [Indx_B, 1], [], false, '', PlotProps);
+        Stats = topoDiff(Data1, Data2, Chanlocs, CLims, StatsP, PlotProps);
+        colorbar off
+
+
+        % afternoon to WMZ
+        Data1 = squeeze(mean(T(:, 4, :, Indx_B, Indx_V), 2, 'omitnan'));
+        Data2 = squeeze(mean(T(:, 11, :, Indx_B, Indx_V), 2, 'omitnan'));
+        subfigure([], [2, 2], [Indx_B, 2], [], false, '', PlotProps);
+        Stats = topoDiff(Data1, Data2, Chanlocs, CLims, StatsP, PlotProps);
+        colorbar off
+    end
+    saveFig(strjoin({TitleTag, 'Topography', Variables{Indx_V}, 'zscore'}, '_'), Results, PlotProps)
+end
+
+figure('Units', 'normalized',  'Position', [0 0 .4 .7])
+plotColorbar('Divergent', CLims, 't-values', PlotProps)
+   saveFig(strjoin({TitleTag, 'Topography', 'colorbar'}, '_'), Results, PlotProps)
