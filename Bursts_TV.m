76133f8bf78d26a7664e2a2d6f8ab5351e5b51c6
Sophia Snipes <code@sophia.science>
2022-08-26 12:46:08 +0200

    mostly works

--- Bursts_TV.m
+++ Bursts_TV.m
@@ -27,11 +27,12 @@ TitleTag = 'Bursts_TV';
 MegatTable_Filename = [Task, 'AllBursts.mat'];
 
 Path = fullfile(Paths.Data, 'EEG', 'Bursts_Table');
-Splits = 0:20:90; % in minutes
+Splits = 0:20:80; % in minutes
+% Splits = [0 90];
 [AllBursts, AllMissing] = loadTVBursts(Path, Splits, Participants, Sessions, fs);
 
-
-
+AllBursts.Session = AllBursts.NewSessions;
+AllBursts.Task = repmat({'TV'}, size(AllBursts, 1), 1);
 %%
 zScore = [false, true];
 % Variables = {'Tot', 'Mean_Coh_amplitude', 'nPeaks'};
@@ -54,14 +55,14 @@ newxLabels = newxLabels(:)';
             for Indx_V = 1:numel(Variables)
 
                 Variable =Variables{Indx_V};
-                Matrix = table2matrixRRT(AllBursts(AllBursts.FreqType == Indx_B, :), AllMissing, Variable, Participants, Sessions, Tasks, Z);
+                Matrix = table2matrixRRT(AllBursts(AllBursts.FreqType == Indx_B, :), ...
+                    AllMissing, Variable, Participants, unique(AllBursts.Session), {'TV'}, Z);
 
                 figure('units', 'centimeters', 'position', [0 0 30 30])
-                plotConfettiSpaghetti(squeeze(Matrix(:, :, Indx_T)), [], xLables, PlotProps.Color.Participants, PlotProps)
+                plotConfettiSpaghetti(squeeze(Matrix(:, :)), [], newxLabels, PlotProps.Color.Participants, PlotProps)
 
-                Title = strjoin({replace(Variable, '_', ' '), Bands{Indx_B}, Tasks{Indx_T}}, ' ');
-                %                 title(Title,  'FontSize', PlotProps.Text.TitleSize)
-                ylabel(YLabels{Indx_T})
+                Title = strjoin({replace(Variable, '_', ' '), Bands{Indx_B}, 'TV'}, ' ');
+                                title(Title,  'FontSize', PlotProps.Text.TitleSize)
 
                 if Z
                     ylabel('z-scores')
--- functions/eeg/loadTVBursts.m
+++ functions/eeg/loadTVBursts.m
@@ -1,9 +1,6 @@
-function [AllBursts, AllMissing] = loadTVBursts(Path, Splits, Participants, Sessions, fs)
+function [BurstTable, Missing] = loadTVBursts(Path, Splits, Participants, Sessions, fs)
 % gets a big table with all the bursts for all the participants and all the
 % sessions, split into smaller windows
-
-AllBursts = table();
-AllMissing = [];
 
 
 TablePath = fullfile(Path, 'TV_AllBursts.mat');
@@ -27,12 +24,46 @@ else
     % duration
     BurstTable.Duration = (BurstTable.All_End - BurstTable.All_Start)/fs;
 
-    % identify main location
-    load('Chanlocs123.mat', 'Chanlocs')
-    BurstTable = burstSpots(BurstTable, Channels.bigROI, Chanlocs, 'bigROI');
+    %     % identify main location
+    %     load('Chanlocs123.mat', 'Chanlocs')
+    %     BurstTable = burstSpots(BurstTable, Channels.bigROI, Chanlocs, 'bigROI');
+
+    NewBurstTable = table();
+    NewBurstTable.nPeaks = BurstTable.nPeaks;
+    NewBurstTable.Band = BurstTable.Band;
+    NewBurstTable.Channel = BurstTable.Channel;
+    NewBurstTable.Sign = BurstTable.Sign;
+    NewBurstTable.Start = BurstTable.Start;
+    NewBurstTable.End = BurstTable.End;
+    NewBurstTable.All_Start = BurstTable.All_Start;
+    NewBurstTable.All_End = BurstTable.All_End;
+    NewBurstTable.globality_bursts = BurstTable.globality_bursts;
+    NewBurstTable.Mean_voltageNeg = BurstTable.Mean_voltageNeg;
+    NewBurstTable.Mean_voltagePos = BurstTable.Mean_voltagePos;
+    NewBurstTable.Mean_periodNeg = BurstTable.Mean_periodNeg;
+    NewBurstTable.Mean_periodPos = BurstTable.Mean_periodPos;
+    NewBurstTable.Mean_amplitude = BurstTable.Mean_amplitude;
+    NewBurstTable.Mean_efficiency = BurstTable.Mean_efficiency;
+    NewBurstTable.Mean_monotonicity = BurstTable.Mean_monotonicity;
+    NewBurstTable.Mean_period = BurstTable.Mean_period;
+    NewBurstTable.Mean_nExtraPeaks = BurstTable.Mean_nExtraPeaks;
+    NewBurstTable.Mean_prominence = BurstTable.Mean_prominence;
+    NewBurstTable.Mean_drsym = BurstTable.Mean_drsym;
+    NewBurstTable.Mean_periodPeakPos = BurstTable.Mean_periodPeakPos;
+    NewBurstTable.Mean_tpsym = BurstTable.Mean_tpsym;
+    NewBurstTable.Type = BurstTable.Type;
+    NewBurstTable.Participant = BurstTable.Participant;
+    NewBurstTable.Session = BurstTable.Session;
+    NewBurstTable.FreqType = BurstTable.FreqType;
+    NewBurstTable.Duration = BurstTable.Duration;
+    NewBurstTable.Mean_Coh_amplitude = BurstTable.Mean_Coh_amplitude;
+
+
+    BurstTable = NewBurstTable;
+
 
     % save
-    save(TablePath, 'BurstTable', 'Missing')
+    save(TablePath, 'BurstTable', 'Missing', '-v7.3')
 end
 
 
