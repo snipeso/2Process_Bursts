d7a7051ff6125f0bfbd3d008fe89d48f3e975588
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-08-31 16:31:38 +0200

    setup new scripts for questionnaire plotting

--- Analyses/Questionnaires.m
+++ Analyses/Questionnaires.m
@@ -1,1 +1,48 @@
 % Plots and stats about questionnaire data
+clear
+clc
+close all
+
+P = getParameters();
+Paths = P.Paths;
+Participants = P.Participants;
+Sessions = P.Sessions;
+PlotProps = P.Manuscript;
+
+Labels = P.Labels;
+StatsP = P.StatsP;
+
+TitleTag = 'Questionnaires';
+
+
+%%% Load data
+[Answers, qLabels, Types] = loadRRT(Paths, Participants, Sessions);
+Questions = fieldnames(Answers);
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Plots
+
+
+%% Figure X, Effect of sleep deprivation on subjective ratings
+
+PlotQuestions = {'KSS', 'SleepPropensity'; 
+    'DifficultyWake', 'DifficultyOddball'; 
+    'Alertness', 'GeneralFeeling'}';
+
+Grid = size(PlotQuestions);
+Indx = 1;
+figure('Units','centimeters','Position',[0 0 PlotProps.W, PlotProps.H*0.5])
+for Indx_1 = 1:Grid(1)
+for Indx_2 = 1:Grid(2)
+Q = PlotQuestions{Indx_1, Indx_2};
+Data = Answers.(Q);
+    A = subfigure([], Grid, [Indx_1, Indx_2], [], true, ...
+        PlotProps.Indexes.Letters{Indx}, PlotProps); Indx = Indx+1;
+
+    
+    title(Q, 'FontSize', PlotProps.Text.TitleSize)
+
+
+end
+end
--- functions/questionnaires/loadRRT.m
+++ functions/questionnaires/loadRRT.m
@@ -5,35 +5,35 @@ function [AllAnswers, Labels, Types] = loadRRT(Paths, Participants, Sessions)
 filename = 'Fixation_All.csv';
 
 qIDs = {'RT_OVR_1_sl1', 'Enjoyable';
-%     'RT_OVR_1_sl2', 'Relaxing';
-%     'RRT_OVT_4', 'Motivation';
-%     'RT_FEE_1.1_sl1', 'Happy';
-%     'RT_FEE_1.1_sl2', 'Sad';
-%     'RT_FEE_1.1_sl3', 'Angry';
-%     'RT_FEE_1.1_sl4', 'Afraid';
-%     'RT_FEE_1.2', 'ExtraFeelings';
-%     'RT_FEE_2', 'Tolerance';
-%     'RT_FEE_3', 'Stress';
-%     'RT_FEE_4_sl1', 'Hungry';
-%     'RT_FEE_4_sl2', 'Thirsty';
-%     'RT_FEE_4_sl3', 'Headache';
-%     'RT_FEE_4_sl4', 'Pain';
-%     'RT_FEE_4_sl5', 'Motivation2';
-%     'RT_FEE_5', 'GeneralFeeling';
-%     'RT_OVR_1_sl1', 'Enjoyment';
-%     'RT_OVR_1_sl2', 'Frustration';
-%     'RT_OVR_2', 'EstimatedDuration';
-%     'RT_OVR_3.1', 'DifficultyFixating';
-%     'RT_OVR_3.2', 'DifficultyWake';
-%     'RT_THO_1', 'Thoughts';
-%     'RT_TIR_1', 'KSS';
-%     'RT_TIR_2_sl1', 'PhysicalTiredness';
-%     'RT_TIR_2_sl2', 'EmotionalTiredness';
-%     'RT_TIR_2_sl3', 'PsychTiredness';
-%     'RT_TIR_2_sl4', 'SpiritTiredness';
-%     'RT_TIR_3.1', 'Sleep';
-%     'RT_TIR_3.2', 'SleepConfidence';
-%     'RT_TIR_4', 'Alertness';
+    'RT_OVR_1_sl2', 'Relaxing';
+    'RRT_OVT_4', 'Motivation';
+    'RT_FEE_1.1_sl1', 'Happy';
+    'RT_FEE_1.1_sl2', 'Sad';
+    'RT_FEE_1.1_sl3', 'Angry';
+    'RT_FEE_1.1_sl4', 'Afraid';
+    'RT_FEE_1.2', 'ExtraFeelings';
+    'RT_FEE_2', 'Tolerance';
+    'RT_FEE_3', 'Stress';
+    'RT_FEE_4_sl1', 'Hungry';
+    'RT_FEE_4_sl2', 'Thirsty';
+    'RT_FEE_4_sl3', 'Headache';
+    'RT_FEE_4_sl4', 'Pain';
+    'RT_FEE_4_sl5', 'Motivation2';
+    'RT_FEE_5', 'GeneralFeeling';
+    'RT_OVR_1_sl1', 'Enjoyment';
+    'RT_OVR_1_sl2', 'Frustration';
+    'RT_OVR_2', 'EstimatedDuration';
+    'RT_OVR_3.1', 'DifficultyFixating';
+    'RT_OVR_3.2', 'DifficultyWake';
+    'RT_THO_1', 'Thoughts';
+    'RT_TIR_1', 'KSS';
+    'RT_TIR_2_sl1', 'PhysicalTiredness';
+    'RT_TIR_2_sl2', 'EmotionalTiredness';
+    'RT_TIR_2_sl3', 'PsychTiredness';
+    'RT_TIR_2_sl4', 'SpiritTiredness';
+    'RT_TIR_3.1', 'Sleep';
+    'RT_TIR_3.2', 'SleepConfidence';
+    'RT_TIR_4', 'Alertness';
     'RT_TIR_5', 'SleepPropensity';
     'RT_TIR_6', 'Focus';
     'RT_oddball', 'DifficultyOddball'};
@@ -55,9 +55,13 @@ Labels = struct();
 Types = struct();
 
 for Indx_Q = 1:size(qIDs, 1)
-[AllAnswers.(qIDs{Indx_Q, 2}), Labels.(qIDs{Indx_Q, 2}), Types.(qIDs{Indx_Q, 2})] = ...
-    qtable2matrix(Table, Participants, Sessions, qIDs{Indx_Q, 1});
+    [AllAnswers.(qIDs{Indx_Q, 2}), Labels.(qIDs{Indx_Q, 2}), Types.(qIDs{Indx_Q, 2})] = ...
+        qtable2matrix(Table, Participants, Sessions, qIDs{Indx_Q, 1});
 
+end
+
+if any(ismember(fieldnames(Labels), 'DifficultyWake'))
+    Labels.DifficultyWake{2} = 'Extremely hard';
 end
 
 
