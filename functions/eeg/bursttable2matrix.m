8500609d2388e27e253a00470429b242fde33cf3
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-09-02 10:58:06 +0200

    change filename, also use durations

--- functions/eeg/bursttable2matrix.m
+++ functions/eeg/bursttable2matrix.m
@@ -1,11 +1,12 @@
-function Matrix = table2matrixRRT(Table, AllMissing, Variable, Participants, Sessions, Tasks, zScore)
+function Matrix = bursttable2matrix(Table, AllMissing, Durations, Variable, Participants, Sessions, Tasks, zScore)
 % extracts from a table all the info needed and converts it to a matrix
 
+%%% gather data
 Matrix = [];
-
 for Indx_T = 1:numel(Tasks)
     BurstTable = Table(strcmp(Table.Task, Tasks{Indx_T}), :);
 
+    % handle weird data types
     if strcmp(Variable, 'Tot')
         M = tabulateTable(BurstTable, 'FreqType', 'tabulate', Participants, Sessions);
         M(isnan(M)) = 0;
@@ -19,15 +20,20 @@ for Indx_T = 1:numel(Tasks)
         M = tabulateTable(BurstTable, Variable, 'mean', Participants, Sessions);
     end
 
+    % set to nan values in which there was no data
     Missing = AllMissing(:, :, Indx_T);
     M(logical(Missing(:))) = nan;
 
     Matrix = cat(3, Matrix, M);
 end
 
+% if the variable is some form of total, and durations are provided,
+% normalize the total by the durations
+if ~isempty(Durations) && ismember({'Tot', 'nPeaks'}, Variable)
+    Matrix = Matrix./Durations;
+end
 
+% normalize the data
 if zScore
     Matrix = zScoreData(Matrix, 'last');
 end
-
-
