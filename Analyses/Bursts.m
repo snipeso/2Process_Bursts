2948f742da179ba1d7f0fdd3b40f63c4a9915735
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-09-02 12:15:04 +0200

    plot topographies (untested)

--- Analyses/Bursts.m
+++ Analyses/Bursts.m
@@ -5,7 +5,6 @@ clear
 clc
 close all
 
-
 P = getParameters();
 Paths = P.Paths;
 Participants = P.Participants;
@@ -14,7 +13,6 @@ PlotProps = P.Manuscript;
 Labels = P.Labels;
 StatsP = P.StatsP;
 Tasks = P.Tasks;
-
 
 Refresh = false;
 fs = 250;
@@ -29,7 +27,7 @@ if exist(fullfile(Path, MegaTable_Filename), 'file') && ~Refresh
     load(fullfile(Path, MegaTable_Filename), 'BurstTable', 'Missing', 'Durations')
 else
     [BurstTable, Missing, Durations] = loadAllBursts(Path, Participants, Sessions, Tasks);
-    save(fullfile(Path, MegaTable_Filename), 'BurstTable', 'Missing', 'Durations')
+    save(fullfile(Path, MegaTable_Filename), 'BurstTable', 'Missing', 'Durations', '-v7.3')
 end
 
 % Use durations in minutes rather than seconds
@@ -43,8 +41,8 @@ Durations = Durations/60;
 
 
 zScore = [false, true];
-Variables = {'Mean_coh_amplitude', 'Tot'};
-YLabels = {'Amplitude (\miV)', '# bursts/min'};
+Variables = {'Mean_coh_amplitude', 'nPeaks'};
+YLabels = {'Amplitude (\miV)', '# oscillations/min'};
 Bands = {'Theta', 'Alpha'};
 YLims = [-3.5 6];
 YLimsZ = [-3.5 6];
@@ -81,7 +79,7 @@ for Indx_B = 1:2
                     PlotProps.Indexes.Letters{Indx}, PlotProps); Indx = Indx+1;
                 plotBrokenSpaghetti(squeeze(Matrix(:, :, Indx_T)), [], YLim, ...
                     StatParameters, PlotProps.Color.Participants, Flip, PlotProps)
-                
+
                 if Indx_V==1
                     title(Tasks{Indx_T}, 'FontSize', PlotProps.Text.TitleSize)
                 end
@@ -98,6 +96,107 @@ end
 
 
 
+%% Assemble topography data
+
+load('Chanlocs123.mat')
+Topos = nan(numel(Participants), numel(Sessions), numel(Tasks), numel(Chanlocs), 2, 2);
+
+
+for Indx_T = 1:numel(Tasks)
+    for Indx_S = 1:numel(Sessions)
+        for Indx_B = 1:2 % loop through frequencies
+            for Indx_P = 1:numel(Participants)
+
+                % load data
+                T = BurstTable(BurstTable.FreqType == Indx_B & strcmp(BurstTable.Participant, Participants{Indx_P}) & ...
+                    strcmp(BurstTable.Session, Sessions{Indx_S}) & strcmp(BurstTable.Task, Tasks{Indx_T}), :);
+
+                if isempty(T)
+                    continue
+                end
+
+                % allign all the channels involved with their average
+                % amplitudes and number of peaks
+                Ch = [T.Coh_Burst_Channels{:}];
+                Amps = [T.Coh_amplitude{:}];
+                Tots = [T.Coh_nPeaks{:}];
+
+                % for each channel, find out how many were involved in
+                % theta and alpha
+                for Indx_Ch = 1:numel(Chanlocs)
+
+                    Ch_Mean = mean(Amps(Ch==Indx_Ch), 'omitnan');
+                    Ch_Tot = sum(Tots(Ch==Indx_Ch), 'omitnan')/Durations(Indx_P, Indx_S, Indx_T);
+
+                    if Ch_Tot == 0
+                        Ch_Mean = 0;
+                    end
+
+                    Topos(Indx_P, Indx_S, Indx_T, Indx_Ch, Indx_B, 1) = Ch_Mean;
+                    Topos(Indx_P, Indx_S, Indx_T, Indx_Ch, Indx_B, 2)  = Ch_Tot;
+                end
+            end
+        end
+    end
+end
+
+% z score each variable separately
+zTopos(:, :, :, :, 1) = zScoreData(squeeze(Topos(:, :, :, :, 1)), 'last');
+zTopos(:, :, :, :, 2) = zScoreData(squeeze(Topos(:, :, :, :, 2)), 'last');
+
+
+
+%% Figure X Burst topopgraphies
+
+Grid = [1, 2];
+miniGrid = [2 2];
+zScore = true;
+Bands = {'Theta', 'Alpha'};
+Variables = {'Mean_coh_amplitude', 'nPeaks'};
+CLabels = {'\miV', 'peaks/min'};
+CLims = [-8 8];
+BL = 4;
+SD = 11;
+
+load('Chanlocs123.mat', 'Chanlocs')
+
+for Indx_T = 1:numel(Tasks)
+
+
+    figure('units', 'centimeters', 'position', [0 0 PlotProps.Figure.W PlotProps.Figure.H*0.6])
+
+    for Indx_B = 1:numel(Bands)
+
+        Space = subaxis(Grid, [1, Indx_B], [], PlotProps.Indexes.Letters{Indx_B}, PlotProps);
+
+        for Indx_V = 1:numel(Variables)
+
+            % plot average across all sessions
+            Data = squeeze(mean(mean(zTopos(:, :, Indx_T, :, Indx_B, Indx_V), 1, 'omitnan'), 2, 'omitnan'));
+
+            A = subfigure(Space, miniGrid, [Indx_V, 1], [], false, {}, PlotProps);
+            plotTopoplot(Data, [], Chanlocs, [], CLabels{Indx_V}, 'Linear', PlotProps)
+
+            if Indx_V ==1
+                title(Bands{Indx_B}, 'FontSize', PlotProps.Text.TitleSize)
+            end
+
+
+            % plot change from main1 to main 8
+            Data1 = squeeze(zTopos(:, BL, Indx_T, :, Indx_B, Indx_V));
+            Data2 = squeeze(zTopos(:, SD, Indx_T, :, Indx_B, Indx_V));
+
+            A = subfigure(Space, miniGrid, [Indx_V, 2], [], false, {}, PlotProps);
+            Stats = topoDiff(Data1, Data2, Chanlocs, CLims, StatsP, PlotProps);
+
+            if Indx_V ==1
+                title('Start vs End', 'FontSize', PlotProps.Text.TitleSize)
+            end
+        end
+    end
+
+    saveFig(strjoin({TitleTag, 'Topographies', Tasks{Indx_T}}, '_'), Paths.Paper, PlotProps)
+end
 
 
 
@@ -108,5 +207,10 @@ end
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Stats
+
+
+%% number of bursts (mean, min/max), average peaks per burst, average coherent channels. Range for overall fewest recording, and overall mostest
 
 
