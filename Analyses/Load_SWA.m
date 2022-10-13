8353ca82b9f454b21a7da2dc7955801df41d320d
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-10-13 22:00:45 +0200

    plot all the sleep power

--- Analyses/Load_SWA.m
+++ Analyses/Load_SWA.m
@@ -14,8 +14,9 @@ Bands.SWA = [1 4];
 
 Hour = 60*60/20;
 MaxHour = 12;
-Channels = P.Channels.All.All;
+Channels = P.Channels;
 
+TitleTag = 'SWA';
 Source = fullfile(Paths.Data, 'EEG', 'Unlocked', 'window4s_full', 'Sleep');
 
 %%% load each dataset
@@ -37,7 +38,7 @@ for Indx_P = 1:numel(Participants)
         % start from first hour
         % get only NREM, identify epochs by hour
         NREM = find(ismember(visnum, [-1 -2 -3]));
-        NREM_Order = 1:size(NREM);
+        NREM_Order = 1:numel(NREM);
         Bins = discretize(NREM_Order, 0:Hour:Hour*MaxHour);
 
         for Indx_H = 1:MaxHour
@@ -52,11 +53,12 @@ for Indx_P = 1:numel(Participants)
             SWA_last(Indx_P, Indx_N, Indx_H, :, :) = squeeze(mean(Power(:, Epochs, :), 2, 'omitnan'));
         end
     end
+    disp(['Finished ' Participants{Indx_P}])
 end
 
 
 % remove entirely NaN hours
-NoHours = isnan(squeeze(mean(mean(mean(mean(SWA_first, 5, 'omitnan'), 4, 'omitnan'), 1), 2)));
+NoHours = isnan(squeeze(mean(mean(mean(mean(SWA_first, 5, 'omitnan'), 4, 'omitnan'), 1, 'omitnan'), 2, 'omitnan')));
 SWA_first(:, :, NoHours, :, :) = [];
 SWA_last(:, :, NoHours, :, :) = [];
 
@@ -65,7 +67,8 @@ zData = zScoreData(SWA_first, 'last');
 chData = meanChData(zData, Chanlocs, Channels.All, 4);
 bData = squeeze(bandData(chData, Freqs, Bands, 'last'));
 
-
+raw_chData = meanChData(SWA_first, Chanlocs, Channels.All, 4);
+raw_bData = squeeze(bandData(raw_chData, Freqs, Bands, 'last'));
 % save data in pool
 
 %
@@ -79,29 +82,102 @@ bData = squeeze(bandData(chData, Freqs, Bands, 'last'));
 
 %% plot NREM spectrums, z-scored, raw
 
+Grid = [1 2];
+BL_Indx = 1;
+xLog = true;
+PlotProps = P.Manuscript;
+
+figure('units', 'centimeters', 'position', [0 0 PlotProps.Figure.Width PlotProps.Figure.Height*0.35])
+A = subfigure([], Grid, [1, 1], [], true, ...
+    PlotProps.Indexes.Letters{1}, PlotProps);
+
+Data = squeeze(raw_chData(:, 1, :, :, :));
+spectrumDiff(Data, Freqs, BL_Indx, [], getColors([1 size(Data, 2)]), xLog, PlotProps, [], P.Labels);
+title('Raw', 'FontSize', PlotProps.Text.TitleSize)
+ylabel('PSD')
 
 
+A = subfigure([], Grid, [1, 2], [], true, ...
+    PlotProps.Indexes.Letters{2}, PlotProps);
+
+Data = squeeze(chData(:, 1, :, :, :));
+spectrumDiff(Data, Freqs, BL_Indx, [], getColors([1 size(Data, 2)]), xLog, PlotProps, [], P.Labels);
+title('Z-scored', 'FontSize', PlotProps.Text.TitleSize)
+saveFig([TitleTag, '_spectrums_BL'], Paths.Paper, PlotProps)
 
 
 %% plot change from 1st h pre, to 1st h post spectrum
 
+xLog = true;
+yLims = [1 40];
+
+figure('units', 'centimeters', 'position', [0 0 PlotProps.Figure.Width PlotProps.Figure.Height*0.35])
+A = subfigure([], Grid, [1, 1], [], true, ...
+    PlotProps.Indexes.Letters{1}, PlotProps);
+
+Data = log(squeeze(raw_chData(:, [2 3], 1, :, :)));
+plotSpectrumMountains(Data, Freqs, xLog, yLims, PlotProps, P.Labels)
+title('Log', 'FontSize', PlotProps.Text.TitleSize)
+ylabel('PSD (log)')
 
 
+A = subfigure([], Grid, [1, 2], [], true, ...
+    PlotProps.Indexes.Letters{2}, PlotProps);
+
+Data = squeeze(chData(:, [2 3], 1, :, :));
+plotSpectrumMountains(Data, Freqs, xLog, yLims, PlotProps, P.Labels)
+title('Z-scored', 'FontSize', PlotProps.Text.TitleSize)
+saveFig([TitleTag, '_spectrums_SD'], Paths.Paper, PlotProps)
 
 
 %% plot change in z-scored SWA across hours for all nights
 
+Grid = [1 numel(Nights)];
+yLims = [-1.5 2.5];
+figure('units', 'centimeters', 'position', [0 0 PlotProps.Figure.Width PlotProps.Figure.Height*0.35])
 
+for Indx_N = 1:numel(Nights)
 
+    %        Data = squeeze(bData(:, Indx_N, :));
+    Data = squeeze(raw_bData(:, Indx_N, :));
+    A = subfigure([], Grid, [1, Indx_N], [], true, ...
+        PlotProps.Indexes.Letters{Indx_N}, PlotProps);
+    plotConfettiSpaghetti(Data, [], [], PlotProps.Color.Participants, yLims, PlotProps)
+    % ylim(yLims)
+end
 
 %% with raw data, plot change from 1st h to last h
 
+% EdgeHours = log(cat(3, SWA_first(:, :, 1, :, :), SWA_last(:, :, 1, :, :)));
+EdgeHours = cat(3, SWA_first(:, :, 1, :, :), SWA_last(:, :, 1, :, :));
+
+EdgeHours = meanChData(EdgeHours, Chanlocs, Channels.All, 4);
+EdgeHours = bandData(EdgeHours, Freqs, Bands, 'last');
+% yLims = [1.5 4.5];
+yLims = [0 250];
+
+figure('units', 'centimeters', 'position', [0 0 PlotProps.Figure.Width PlotProps.Figure.Height*0.35])
+
+for Indx_N = 1:numel(Nights)
+
+    Data = squeeze(EdgeHours(:, Indx_N, :, :, :));
+    A = subfigure([], Grid, [1, Indx_N], [], true, PlotProps.Indexes.Letters{Indx_N}, PlotProps);
+    plotConfettiSpaghetti(Data, [], [], PlotProps.Color.Participants, yLims, PlotProps)
+    ylim(yLims)
+end
 
 
 
+%%
 
+    Start = squeeze(EdgeHours(:, 1, 1, :, :));
+    EndPre = squeeze(EdgeHours(:, 2, 2, :, :));
+    StartPost = squeeze(EdgeHours(:, 3, 1, :, :));
+    figure
 
+%     Data = [StartPost, EndPre]./Start;
 
-
-
+% Data = [StartPost, Start]./Start;
+Data = [End, Start]./Start;
+ plotConfettiSpaghetti(Data, [], [], PlotProps.Color.Participants, yLims, PlotProps)
 
