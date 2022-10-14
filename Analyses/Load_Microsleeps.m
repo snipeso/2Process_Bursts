1d04596645f4552dde7306fc7464a47780a97246
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2022-10-14 13:28:04 +0200

    save microsleeps to pool

---
+++ Analyses/Load_Microsleeps.m
@@ -0,0 +1,75 @@
+clear
+clc
+close all
+
+P = analysisParameters();
+Paths = P.Paths;
+Participants = P.Participants;
+Sessions = P.Sessions;
+PlotProps = P.Manuscript;
+Labels = P.Labels;
+StatsP = P.StatsP;
+Tasks = {'Fixation', 'Oddball'};
+Colors = P.TaskColors(1:2, :);
+Refresh = true;
+
+TitleTag = 'Microsleeps';
+
+% load in data table of all eye closures
+AllBlinks = fullfile(Paths.Data, 'Eyes', 'Blinks', 'AllBlinks.mat');
+BlinkLocation = fullfile(Paths.Preprocessed, 'Pupils', 'Raw');
+if Refresh || ~exist(AllBlinks, 'file')
+    [BlinkTable, RecordingDurations, Confidence] = getBlinks(BlinkLocation, Participants, Sessions, Tasks);
+
+    if ~exist('E:\Data\Final\Eyes\Blinks', 'dir')
+        mkdir('E:\Data\Final\Eyes\Blinks')
+    end
+
+    save(AllBlinks, 'BlinkTable', 'RecordingDurations', 'Confidence')
+else
+    load(AllBlinks, 'BlinkTable', 'RecordingDurations', 'Confidence')
+end
+
+
+MicrosleepThreshold = 1;
+BlinkThreshold = 1; % anything less is a blink, more is a microsleep
+
+
+% save data to matrix
+TotBlinks = nan(numel(Participants), numel(Sessions), numel(Tasks)); % blinks per minute
+MicrosleepDur = TotBlinks; % percent time microsleeping
+
+for Indx_P = 1:numel(Participants)
+    for Indx_S = 1:numel(Sessions)
+        for Indx_T = 1:numel(Tasks)
+
+            Dur = squeeze(RecordingDurations(Indx_P, Indx_S, Indx_T))/60; % in minutes
+            T = BlinkTable(strcmp(BlinkTable.Participant, Participants{Indx_P}) & ...
+                strcmp(BlinkTable.Session, Sessions{Indx_S}) & strcmp(BlinkTable.Task, Tasks{Indx_T}), :);
+
+            TotBlinks(Indx_P, Indx_S, Indx_T) = nnz(T.Duration<BlinkThreshold)/Dur;
+            MicrosleepDur(Indx_P, Indx_S, Indx_T) = sum(T.Duration(T.Duration>=MicrosleepThreshold))/Dur;
+        end
+    end
+end
+
+
+%% save to pool
+
+% blinks
+Data = TotBlinks; % P x S x T
+save(fullfile(Paths.Pool, [TitleTag, '_nBlinks.mat']), 'Data')
+
+
+% microsleeps
+Data = MicrosleepDur; % P x S x T
+save(fullfile(Paths.Pool, [TitleTag, '_prcntMicrosleep.mat']), 'Data')
+
+
+
+
+
+
+
+
+
