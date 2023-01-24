df9e0fb2f64ae5446234710d611998a5d08b5f7a
Sophia Snipes <sophia.snipes@kispi.uzh.ch>
2023-01-24 14:14:14 +0100

    cleared junk

--- Analyses/Plot_Pupillometry.m
+++ Analyses/Plot_Pupillometry.m
@@ -106,6 +106,8 @@ end
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Stats
 
 %% special comparison of oddball vs fixation for 23:00 recordings
 clc
@@ -123,49 +125,3 @@ for Indx_S = 1:numel(S_Indexes)
 end
 
 
-% 
-% %% plot response to targets
-% 
-% 
-% Grid = [3 4];
-% % YLims = [-.4 1];
-% YLims = [-3.3 5];
-% Coordinates = {[1 1], [1 2], [1 3], [1, 4];
-%     [2 1], [2 2], [2 3], [2, 4];
-%     [3 1], [3 2], [3 3], [3, 4]}';
-% 
-% % Colors = [0.6 0.6 0.6; getColors(1, '', 'red')];
-% Colors = PlotProps.Color.Participants;
-% 
-% Legend = {'Standard', 'Target'};
-% 
-% figure('units', 'centimeters', 'position', [0 0 PlotProps.Figure.Width PlotProps.Figure.Height*0.6])
-% AllN = nan(1, numel(Sessions));
-% for Indx_S = 1:numel(Sessions)
-% 
-%     Data = squeeze(zTimecourse(:, Indx_S, :, :));
-%     N = nnz(~isnan(Data(:, 1, 1))); % number of participants included
-% 
-%     AllN(Indx_S) = N;
-%     Coordinate = Coordinates{Indx_S};
-%     A = subfigure([], Grid, Coordinate, [], true, ...
-%         '', PlotProps);
-%     plotAngelHair(t, Data, Colors, Legend, PlotProps)
-%     title([XLabels{Indx_S}, ' (n=', num2str(N), ')'])
-%     ylim(YLims)
-%     if Indx_S>1
-%         legend off
-%     else
-%         set(legend, 'ItemTokenSize', [10 10], 'location', 'southwest')
-%     end
-% 
-%     if Indx_S>8
-%         xlabel('t(s)')
-%     end
-% 
-%     if Coordinate(2) ==1
-%         ylabel('diameter (z-scored)')
-%     end
-% end
-% 
-% saveFig([TitleTag, 'EvokedResponse'], Paths.Paper, PlotProps)
