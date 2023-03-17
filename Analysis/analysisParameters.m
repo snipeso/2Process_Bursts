89870f50290cb66d529516abd24fa1f3fc5f7d28
Sophia Snipes <snipeso@users.noreply.github.com>
2023-03-17 18:56:09 +0100

    fix conflict carrots

--- Analysis/analysisParameters.m
+++ Analysis/analysisParameters.m
@@ -166,12 +166,9 @@ P.Channels = Channels;
 
 Bands.Theta = [4 8];
 Bands.Alpha = [8 12];
-<<<<<<< Updated upstream
-
-=======
 % Bands.End = [1, 1.1];
 % Bands.Delta = [1 4];
->>>>>>> Stashed changes
+
 
 P.Bands = Bands;
 clc
