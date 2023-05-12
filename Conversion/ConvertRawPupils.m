6118bf1d486e7d7430dadcb494bf4e49de778e98
Sophia Snipes <snipeso@users.noreply.github.com>
2023-05-12 15:35:51 +0200

    script for converting pupil data

---
+++ Conversion/ConvertRawPupils.m
@@ -0,0 +1,36 @@
+% converts EEG to EDF
+
+clear
+clc
+close all
+
+Refresh = false;
+Task = 'Fixation';
+Source = fullfile('E:\Data\Preprocessed\Pupils\Raw\', Task);
+
+Destination = fullfile('E:\Public\2Process_Bursts\', Task);
+
+if ~exist(Destination, 'dir')
+    mkdir(Destination)
+end
+
+Files = getContent(Source);
+
+for Indx_F = 1:numel(Files)
+    Filename = Files{Indx_F};
+    NewFilename = replace(Filename, '.mat', '.csv');
+
+    if ~Refresh && exist(fullfile(Destination, NewFilename), 'file')
+        disp(['already did ', NewFilename])
+        continue
+    end
+
+    load(fullfile(Source, Filename), 'Pupil', 'Annotations')
+
+    writetable(Pupil, fullfile(Destination, NewFilename))
+
+    AnnotationFilename = replace(NewFilename, 'Pupils', 'Annotations');
+
+end
+
+
