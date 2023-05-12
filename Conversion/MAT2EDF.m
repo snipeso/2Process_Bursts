13d6958b516c138c5d8f5cc0a3b80c9b2d5c8df1
Sophia Snipes <snipeso@users.noreply.github.com>
2023-05-12 15:21:13 +0200

    script to convert EEG to EDF

---
+++ Conversion/MAT2EDF.m
@@ -0,0 +1,44 @@
+% converts EEG to EDF
+
+clear
+clc
+close all
+
+Refresh = false;
+Task = 'Fixation';
+Source = fullfile('E:\Data\Preprocessed\Clean\Waves\', Task);
+
+Destination = fullfile('E:\Public\2Process_Bursts\EEG', Task);
+MetadataDestination = fullfile('E:\Public\2Process_Bursts\Metadata');
+
+if ~exist(Destination, 'dir')
+    mkdir(Destination)
+end
+
+if ~exist(MetadataDestination, 'dir')
+    mkdir(MetadataDestination)
+end
+
+Files = getContent(Source);
+
+for Indx_F = 1:numel(Files)
+    Filename = Files{Indx_F};
+    NewFilename = replace(Filename, '.mat', '.edf');
+
+    if ~Refresh && exist(fullfile(Destination, NewFilename), 'file')
+        disp(['already did ', NewFilename])
+        continue
+    end
+
+    load(fullfile(Source, Filename), 'EEG')
+
+    EEG = eeg_checkset(EEG);
+    writeeeg(fullfile(Destination, NewFilename), EEG.data, EEG.srate, ...
+        'TYPE', 'EDF', 'EVENT', EEG.event, 'Label', {EEG.chanlocs.labels});
+end
+
+Chanlocs = EEG.chanlocs;
+
+ChanlocsTable = struct2table(Chanlocs);
+writetable(ChanlocsTable, fullfile(MetadataDestination, 'EEG_Chanlocs.csv'))
+
